ee5d79a36964fdb2f9c9f534e90e86ae07a14151
ChrisJonesBTS <chris@beetheswarm.com>
2015-09-22 05:08:20 -0400

    initial commit

---
+++ Assets/EnableOnLoad.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnableOnLoad : MonoBehaviour {
+
+   public GameObject[] gameObjectsToEnable;
+	// Use this for initialization
+	void Start () {
+        foreach (GameObject gameObject in gameObjectsToEnable) {
+            gameObject.SetActive(true);
+        }
+	}
+}
